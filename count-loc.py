ec0dea2da083bd675fe9ad7d349e8bf83ccbcc70
Tazmondo <fondcoolman@outlook.com>
2023-09-21 20:24:45 +0100

    Add count lines of code script

---
+++ count-loc.py
@@ -0,0 +1,17 @@
+import subprocess
+
+x = (subprocess.check_output(['git', 'ls-files'])).decode('utf-8')
+
+filenames = x.strip().split("\n")
+validExts = ['lua']
+
+loc = 0
+
+for filename in filenames:
+    if filename.split(".")[-1] in validExts:
+        with open(filename, "r") as f:
+            length = len(f.readlines())
+            loc += length
+            print(f'{filename} : {length}')
+
+print(loc)
