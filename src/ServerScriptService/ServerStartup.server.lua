0c3d0fb86669818a0dbbef8a195ce2a2cd9dfcb4
Tazmondo <fondcoolman@outlook.com>
2023-10-03 02:22:52 +0100

    refactor server startup

--- src/ServerScriptService/ServerStartup.server.lua
+++ src/ServerScriptService/ServerStartup.server.lua
@@ -1,10 +1,8 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerStorage = game:GetService("ServerStorage")
 local Loader = require(ReplicatedStorage.Packages.Loader)
-local Red = require(ReplicatedStorage.Packages.Red)
 
 -- As client relies on this folder existing, we can just make it here to ensure they don't wait forever
-Red.Server("game"):Folder()
 
 local loaded = Loader.LoadChildren(ServerStorage.Modules)
 -- Loader.SpawnAll(loaded, "Start")
