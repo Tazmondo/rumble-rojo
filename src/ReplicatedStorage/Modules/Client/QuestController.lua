f38b5c6396b1e7119c195270b15e569f8e925e74
Tazmondo <fondcoolman@outlook.com>
2023-10-15 16:52:13 +0100

    display current quests

---
+++ src/ReplicatedStorage/Modules/Client/QuestController.lua
@@ -0,0 +1,99 @@
+-- Handles rendering of quest UI, though its visibility is controlled by the UI controller
+local QuestController = {}
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local DataController = require(script.Parent.DataController)
+
+local ClaimQuestEvent = require(ReplicatedStorage.Events.Quest.ClaimQuestEvent):Client()
+local RefreshQuestsEvent = require(ReplicatedStorage.Events.Quest.RefreshQuestsEvent):Client()
+
+local Player = Players.LocalPlayer
+
+local PlayerGui = Player:WaitForChild("PlayerGui") :: PlayerGui
+
+local QuestUI = (PlayerGui:WaitForChild("MainUI") :: any).Interface.Quest
+
+local questListFrame = QuestUI.Content.Rows.ScrollingFrame
+
+local template = questListFrame.Template
+template.Parent = nil -- So we can clear children without destroying this template
+
+function RenderQuests()
+	local data = DataController.GetLocalData():Await().Private
+	local quests = data.Quests
+
+	for i, child in ipairs(questListFrame:GetChildren()) do
+		if child:IsA("Frame") then
+			child:Destroy()
+		end
+	end
+
+	for i, quest in ipairs(quests) do
+		local newQuest = template:Clone()
+
+		newQuest.Title.Text = quest.Text
+
+		local progress = math.clamp(quest.CurrentNumber / quest.RequiredNumber, 0, 1)
+		local progressBar = newQuest.Details.OuterBar.InnerBar :: Frame
+
+		progressBar.Size = UDim2.fromScale(progress, 1)
+
+		local rewardLabel = newQuest.Details.Progress.Reward.GBucksCount
+		rewardLabel.Text = quest.Reward
+
+		local progressFrame = newQuest.Details.Progress
+
+		local claimable = quest.CurrentNumber >= quest.RequiredNumber and not quest.Claimed
+		if claimable then
+			progressFrame.Claim.Visible = true
+			progressFrame.Progress.Visible = false
+			progressFrame.Reward.Visible = false
+
+			progressFrame.Claim.Activated:Connect(function()
+				ClaimQuestEvent:Fire(i)
+				quest.Claimed = true
+				RenderQuests()
+			end)
+		else
+			progressFrame.Claim.Visible = false
+			progressFrame.Progress.Visible = true
+			progressFrame.Reward.Visible = true
+
+			if quest.Claimed then
+				progressFrame.Progress.Claimed.Visible = true
+				progressFrame.Progress.Progress.Visible = false
+			else
+				progressFrame.Progress.Claimed.Visible = false
+				progressFrame.Progress.Progress.Visible = true
+
+				local progressText
+				if quest.RequiredNumber > 1000 then
+					local current = math.floor(quest.CurrentNumber / 1000)
+					local required = math.floor(quest.RequiredNumber / 1000)
+					if current == 0 then
+						progressText = string.format("0/%ik", required) -- No k when 0
+					else
+						progressText = string.format("%ik/%ik", current, required)
+					end
+				else
+					progressText = quest.CurrentNumber .. "/" .. quest.RequiredNumber
+				end
+				progressFrame.Progress.Progress.Text = progressText
+			end
+		end
+
+		newQuest.Visible = true
+		newQuest.Parent = questListFrame
+	end
+end
+
+function QuestController.Initialize()
+	DataController.LocalDataUpdated:Connect(function()
+		RenderQuests()
+	end)
+end
+
+QuestController.Initialize()
+
+return QuestController
