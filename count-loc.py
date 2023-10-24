4b83488473f6ae8bbe1e74b541426f8c3fffe2d6
Tazmondo <fondcoolman@outlook.com>
2023-10-25 00:54:14 +0100

    update file counter script

--- count-loc.py
+++ count-loc.py
@@ -6,12 +6,14 @@ filenames = x.strip().split("\n")
 validExts = ['lua']
 
 loc = 0
+fileCount = 0
 
 for filename in filenames:
-    if filename.split(".")[-1] in validExts and not "Iris" in filename and not "ProfileService" in filename:
+    if filename.split(".")[-1] in validExts and not "Iris" in filename and not "ProfileService" in filename and not "Spring" in filename:
         with open(filename, "r") as f:
             length = len(f.readlines())
             loc += length
             print(f'{filename} : {length}')
+            fileCount += 1
 
-print(loc)
+print(f"\nFile Count: {fileCount}\nLines of Code: {loc}")
